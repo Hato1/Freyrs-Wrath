4ad369684848c9d9831ec63b514a6017ac0df6ce
Torben Klausen <tkl34@uclive.ac.nz>
2021-08-28 01:26:33 +1200

    worlds have shopes

--- shop.py
+++ shop.py
@@ -1,13 +1,27 @@
+import pygame as pg
+
 class Shop:
 
-    def __init__(self, power_list, player):
-        self.power_list = power_list
-        self.player = player
+    def __init__(self):
         self.open = False
-        self.shop_background = None
-
-    def open(self):
-        pass
+        self.shop = pg.Surface((30, 30))
+        self.shop = self.shop.convert()
 
     def draw_shop(self):
-        self.shop_background.fill((100, 250, 250))
+        if self.open:
+            self.draw_open_shop()
+        else:
+            self.draw_closed_shop()
+
+    def draw_open_shop(self):
+        self.shop.fill((10, 150, 50))
+
+    def draw_closed_shop(self):
+        self.shop.fill((10, 200, 250))
+
+    def set_openness(self, open):
+        if open == False:
+            self.shop = pg.Surface((30, 30))
+        else:
+            self.shop = pg.Surface((150, 30))
+        self.shop = self.shop.convert()
