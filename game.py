c198da7faa4839c2054af4fb48c92e19aaf06327
Torben Klausen <tkl34@uclive.ac.nz>
2021-08-28 00:20:48 +1200

    refactored to get rid of 'set state' stuff

--- game.py
+++ game.py
@@ -20,11 +20,11 @@ GREEN = (20, 239, 20)
 BLACK = (0, 0, 0)
 GAME_NAME = "Name of the Game"
 
+
 class Game:
 
     def __init__(self, state=MENU):
         self.game_state = state
-        self.state_change_processed = False
         self.menu_surface = None  # init in setup_game
         self.screen = None  # init in setup_game
         self.soundtrack = None
@@ -39,13 +39,12 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode((512, 288), pg.SCALED | pg.RESIZABLE)
         pg.display.set_caption(GAME_NAME)
-        self.set_state_menu()
+        self.draw_menu_background()
 
         self.soundtrack = load_sound("Fishing song.mp3")
-        #self.soundtrack.play(-1)
+        # self.soundtrack.play(-1)
 
-
-    def set_state_menu(self):
+    def draw_menu_background(self):
         # Create The Menu
         self.menu_surface = pg.Surface(self.screen.get_size())
         self.menu_surface = self.menu_surface.convert()
@@ -67,8 +66,8 @@ class Game:
 
         text_by = font_team.render("by", 1, (220, 20, 60))
         textpos_by = text_by.get_rect(centerx=self.menu_surface.get_width() / 2,
-                                          centery=self.menu_surface.get_height() / 2.2)
-        self.menu_surface.blit(text_by, textpos_by )
+                                      centery=self.menu_surface.get_height() / 2.2)
+        self.menu_surface.blit(text_by, textpos_by)
 
         font_space_to_begin = pg.font.Font(os.path.join(DATA_DIR, 'AmaticSC-Regular.ttf'), 16 * 3)
         text_space_to_begin = font_space_to_begin.render("Press Spacebar to Start", 1, (220, 20, 60))
@@ -76,20 +75,17 @@ class Game:
                                                               centery=self.menu_surface.get_height() / 1.2)
         self.menu_surface.blit(text_space_to_begin, textpos_space_to_begin)
 
-    def set_state_game(self):
+    def draw_game_background(self):
         half_screen_width = self.screen.get_size()[0] / 2
         screen_height = self.screen.get_size()[1]
-        self.screen.blit(self.p1.world, (0,0))
-        self.screen.blit(self.p2.world, (half_screen_width+1, 0))
+        self.screen.blit(self.p1.world, (0, 0))
+        self.screen.blit(self.p2.world, (half_screen_width + 1, 0))
         divider = pg.Surface((1, screen_height))
         divider = divider.convert()
         divider.fill(BLACK)
         self.screen.blit(divider, (half_screen_width, 0))
-
         pg.display.flip()
 
-    def set_state_end(self):
-        pass
 
     def main(self):
         """this function is called when the program starts.
@@ -105,15 +101,13 @@ class Game:
         clock = pg.time.Clock()
         allsprites = pg.sprite.RenderPlain(())
 
-        self.set_state_menu()
+        self.draw_menu_background()
 
         # Main Loop
         going = True
         while going:
             clock.tick(60)
             # Handle Input Events
-            if not self.state_change_processed:
-                self.handle_state_change()
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
@@ -134,15 +128,10 @@ class Game:
                 self.menu_loop()
             elif self.game_state == GAME:
                 self.game_loop()
-                self.p1.update_world()
-                self.p2.update_world()
-                self.set_state_game()
             elif self.game_state == END:
                 self.end_loop()
 
             allsprites.update()
-
-
 
             # Draw Everything
             allsprites.draw(self.screen)
@@ -159,8 +148,6 @@ class Game:
             self.process_end_event(event)
 
     def process_menu_event(self, event):
-        self.screen.blit(self.menu_surface, (0, 0))
-
         if event.type == pg.KEYDOWN and event.key == pg.K_SPACE and self.game_state == MENU:
             self.game_state = GAME
             button_sound = load_sound("button_sound.mp3")
@@ -174,24 +161,15 @@ class Game:
         pass
 
     def menu_loop(self):
-        pass
+        self.screen.blit(self.menu_surface, (0, 0))
 
     def game_loop(self):
         self.p1.update_world()
         self.p2.update_world()
+        self.draw_game_background()
 
     def end_loop(self):
         pass
-
-    def handle_state_change(self):
-        if self.game_state == MENU:
-            self.set_state_menu()
-        elif self.game_state == GAME:
-            self.set_state_game()
-        elif self.game_state == END:
-            self.set_state_end()
-
-        self.state_change_processed = True
 
 
 # Game Over
