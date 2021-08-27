370ebd90f183bd0bb424f692ba818afeaab0762e
Torben Klausen <tkl34@uclive.ac.nz>
2021-08-27 22:31:38 +1200

    funcky names

--- world.py
+++ world.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import os
+
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 DATA_DIR = os.path.join(MAIN_DIR, "data")
 
@@ -11,38 +12,30 @@ class World:
 
         self.world = pg.Surface(self.dims)
         self.world = self.world.convert()
-        self.drawWorld()
+        self.draw_world()
 
         self.money = 100
         self.font_money = pg.font.Font(os.path.join(DATA_DIR, 'Amatic-Bold.ttf'), 36 * 3)
         self.text_money = self.font_money.render(str(self.money), 1, (220, 20, 60))
-        self.updateMoney()
+        self.update_money()
 
-
-
-    def updateWorld(self):
+    def update_world(self):
         self.money -= 1
-        self.updateMoney()
+        self.update_money()
 
-
-    def getSurface(self):
+    def get_surface(self):
         return self.world
 
-
-    def drawWorld(self):
+    def draw_world(self):
         self.world.fill((100, 250, 250))
 
-    def updateMoney(self):
-        textpos_money = self.text_money.get_rect(centerx = self.world.get_width() / 2, centery = self.world.get_height() / 4)
+    def update_money(self):
+        textpos_money = self.text_money.get_rect(centerx=self.world.get_width() / 2,
+                                                 centery=self.world.get_height() / 4)
         self.world.blit(self.text_money, textpos_money)
 
-
-
-
-
-    def getGameState(self):
+    def get_game_state(self):
         return
-
 
 #     Screen.player = Entity()
 #     Screen.entities = []
